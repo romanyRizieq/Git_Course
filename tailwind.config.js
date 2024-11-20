cbd0b8ba4c38c65ca997a382ca3ac25a5fccaa4b
Romany <romanyfarouk9999@gmail.com>
2024-11-20 20:08:28 +0200

    created project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust paths based on your project structure
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
